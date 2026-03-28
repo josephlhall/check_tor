abbeba40315f5bf761f93d20b6eecae319ebe527
joehall <joehall@gmail.com>
2026-03-28 07:13:35 -0400

    increased timeout to 60s

--- check_tor.zsh
+++ check_tor.zsh
@@ -43,7 +43,7 @@ while IFS= read -r url || [[ -n "$url" ]]; do
         -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" \
         -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8" \
         -H "Accept-Language: en-US,en;q=0.5" \
-        --socks5-hostname "$TOR_PROXY" --max-time 30 "$url")
+        --socks5-hostname "$TOR_PROXY" --max-time 60 "$url")
     
     curl_exit_code=$?
     
